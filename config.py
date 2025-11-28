006f05cf45161ded88a2e2b911443a39c8b69846
ribilraj <ribilrajdhakar@gmail.com>
2025-11-28 19:01:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "24037760"
+api_hash = "dccc3070f1c12ab155011f14c3d6ae6a"
+bot_token = "8586286995:AAE9Cn53xNTjG-TzcbUHJfl0eAQVL496krc"
+auth_users = [7834875502]
+sudo_users = [7834875502]
